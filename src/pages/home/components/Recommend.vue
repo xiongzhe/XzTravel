b8096c49164f541fff0909367ce52a45794a7735
mac <xiong@mljia.cn>
2018-05-28 15:52:26 +0800

    weekend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item" v-for="item of recommendList" :key="item.id">
+        <img class="item-img"
+             :src="item.imgUrl">
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: 'HomeRecommend',
+    data () {
+      return {
+        recommendList: [{
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1505/55/55978d709ae63850.water.jpg_200x200_5120b9d6.jpg',
+          title: '世界之窗',
+          desc: '各个国家建筑设计，游走于世界之中'
+        }, {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1505/55/55978d709ae63850.water.jpg_200x200_5120b9d6.jpg',
+          title: '世界之窗',
+          desc: '各个国家建筑设计，游走于世界之中'
+        }, {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1505/55/55978d709ae63850.water.jpg_200x200_5120b9d6.jpg',
+          title: '世界之窗',
+          desc: '各个国家建筑设计，游走于世界之中'
+        }, {
+          id: '0004',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1505/55/55978d709ae63850.water.jpg_200x200_5120b9d6.jpg',
+          title: '世界之窗',
+          desc: '各个国家建筑设计，游走于世界之中'
+        }]
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/mixins.styl"
+
+  .title
+    margin-top .2rem
+    line-height .6rem
+    background #eee
+    text-indent .2rem
+
+  .item
+    overflow hidden
+    display flex
+    height 1.9rem
+    .item-img
+      width 1.7rem
+      height 1.7rem
+      padding .1rem
+    .item-info
+      flex 1
+      padding .1rem
+      min-width 0
+      .item-title
+        line-height .54rem
+        font-size .32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        color #ccc
+        ellipsis()
+      .item-button
+        line-height .44rem
+        margin-top .16rem
+        background: #ff9300
+        padding: 0 .1rem
+        border-radius .06rem
+        color white
+
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item" v-for="item of recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: 'HomeWeekend',
+    data () {
+      return {
+        recommendList: [{
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1507/d9/2ddc7652fdba16.jpg_r_640x214_5e62f1a3.jpg',
+          title: '世界之窗',
+          desc: '各个国家建筑设计，游走于世界之中'
+        }, {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1602/88/bf120edeaea383.jpg_r_640x214_f8591f7b.jpg',
+          title: '世界之窗',
+          desc: '各个国家建筑设计，游走于世界之中'
+        }, {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/eb/e4b181a6957c36.jpg_r_640x214_4abbdbc0.jpg',
+          title: '世界之窗',
+          desc: '各个国家建筑设计，游走于世界之中'
+        }, {
+          id: '0004',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1603/df/e0ab9dfce8786e.jpg_r_640x214_5e49a793.jpg',
+          title: '世界之窗',
+          desc: '各个国家建筑设计，游走于世界之中'
+        }]
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/mixins.styl"
+
+  .title
+    margin-top .2rem
+    line-height .6rem
+    background #eee
+    text-indent .2rem
+
+  .item-img-wrapper
+    overflow hidden
+    height 0
+    padding-bottom 33.9%
+    .item-img
+      width 100%
+
+  .item-info
+    padding .1rem
+    .item-title
+      line-height .54rem
+      font-size .32rem
+      ellipsis()
+    .item-desc
+      line-height: .4rem
+      color #ccc
+      ellipsis()
+
+</style>
