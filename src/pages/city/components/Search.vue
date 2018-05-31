ce5c31c99c8bd1e5862fd4daec3c0f61981a0951
mac <xiong@mljia.cn>
2018-05-31 16:55:28 +0800

    Search finish

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,58 @@
 <template>
-  <div class="search">
-    <input class="search-input" type="text" placeholder="输入城市名或拼音">
+  <div>
+    <div class="search">
+      <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音">
+    </div>
+    <div class="search-content" ref="search" v-show="keyword">
+      <ul>
+        <li class="search-item" v-for="item of list" :key="item.id">{{item.name}}</li>
+        <li class="search-item" v-show="hasNoData">没有找到匹配数据</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+  import Bscroll from 'better-scroll'
+
   export default {
     name: 'CitySearch',
+    props: {
+      cities: Object
+    },
+    data () {
+      return {
+        keyword: '',
+        list: [],
+        timer: null
+      }
+    },
+    computed: {
+      hasNoData () {
+        return !this.list.length
+      }
+    },
+    mounted () {
+      this.scroll = new Bscroll(this.$refs.search)
+    },
+    watch: {
+      keyword () {
+        if (this.timer) {
+          clearTimeout()
+        }
+        this.timer = setTimeout(() => {
+          const result = []
+          for (let i in this.cities) {
+            this.cities[i].forEach((value) => {
+              if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+                result.push(value)
+              }
+            })
+          }
+          this.list = result
+        }, 100)
+      }
+    }
   }
 </script>
 
@@ -25,4 +71,21 @@
       color #666
       box-sizing border-box
       padding 0 .1rem
+
+  .search-content
+    z-index 1
+    overflow hidden
+    position absolute
+    width 100%
+    top 1.58rem
+    bottom 0
+    left 0
+    right 0
+    background #eee
+    .search-item
+      line-height .62rem
+      padding-left .2rem
+      color #666
+      background #fff
+      border-bottom .02rem solid #eee
 </style>
