c0951c92bdc5a76167a5a65fd17548d1432546aa
mac <xiong@mljia.cn>
2018-06-05 15:26:30 +0800

    动画效果封装

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -10,12 +10,15 @@
         </div>
       </div>
     </div>
-    <common-gallary :imgs="bannerImgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+    <fade-animation>
+      <common-gallary :imgs="bannerImgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+    </fade-animation>
   </div>
 </template>
 
 <script>
   import CommonGallary from 'common/gallary/Gallary'
+  import FadeAnimation from 'common/fade/FadeAnimation'
 
   export default {
     name: 'DetailBanner',
@@ -38,7 +41,8 @@
       }
     },
     components: {
-      CommonGallary
+      CommonGallary,
+      FadeAnimation
     }
   }
 </script>
